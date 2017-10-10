4f50834065a8e3c8ae276f4d209dff1bdc42bb98
mlbors <matyas.bors@gmail.com>
2017-10-10 21:54:42 +0200

    Add url validation

--- index.js
+++ index.js
@@ -1,25 +1,105 @@
+/**
+ * freeCodeCamp - Back End Development Certification - API Projects
+ * URL Shortener Microservice
+ * 
+ * @author MLBORS
+ * @version 1.0.0.0
+ * @since 2017.10.10
+ * @for freeCodeCamp
+ */
+
+/*******************/
+/***** IMPORTS *****/
+/*******************/
+
 const http = require('http')
 const express = require('express')
-const assert = require('assert')
-const MongoClient = require('mongodb').MongoClient
+const validUrl = require('valid-url')
+const mongodb = require('mongodb')
 
-const app = express('mongodb')
+/************************************************************/
+/************************************************************/
+
+/********************/
+/***** SETTINGS *****/
+/********************/
 
 const hostname = '0.0.0.0'
 const port = process.env.PORT || 3000
+const dbURL = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/shortner-microservice';
 
-const url = 'mongodb://localhost:27017/shortner-microservice';
+/************************************************************/
+/************************************************************/
 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server")
-  db.close();
-});
+/********************/
+/***** DATABASE *****/
+/********************/
 
-app.get('/', (req, res) => {
-  res.end('Hello World!')
-})
+const MongoClient = mongodb.MongoClient
 
-app.listen(port, hostname, function(){
-  console.log('Server running at http://${' + hostname + '}:${' + port + '}/')
+/************************************************************/
+/************************************************************/
+
+/***************/
+/***** APP *****/
+/***************/
+
+const app = express('mongodb')
+
+MongoClient.connect(dbURL, function(err, db) {
+
+  if (err) {
+    throw new Error('Database failed to connect!');
+  } else {
+      console.log('Successfully connected to MongoDB on port 27017.');
+  }
+
+  /************************************************************/
+  /************************************************************/
+
+  /******************/
+  /***** ROUTES *****/
+  /******************/
+
+  /*****/
+  /***** HOME *****/
+  /*****/
+
+  app.get('/', (req, res) => {
+    res.end('Hello World!')
+  })
+
+  /************************************************************/
+  /************************************************************/
+
+  /*****/
+  /***** NEW *****/
+  /*****/
+
+  app.get("/new/*", (req,res) => {
+
+    let url = req.params[0]
+    console.log(url)
+
+    if (validUrl.isUri(url)) {
+      console.log('valid url')
+      res.end('Valid url')
+    } else {
+      console.log('invalid url')
+      res.end('Invalid url')
+    }
+
+  })
+    
+  /************************************************************/
+  /************************************************************/
+
+  /******************/
+  /***** LISTEN *****/
+  /******************/
+
+  app.listen(port, hostname, () => {
+    console.log('Server running at http://${' + hostname + '}:${' + port + '}/')
+  })
+
 })
